fe4e1967e3d19277b2e6f528131174d14322a3d6
Eduardo Puermas <eduardopuermas@hotmail.com>
2020-04-03 12:33:06 +0000

    layout-set

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -6,19 +6,42 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 export function Home() {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<div className="navbar">
+				<div className="logo"> </div>
+				<ul className="menu">
+					<li className="img" />
+					<li className="img" />
+					<li className="img" />
+				</ul>
+			</div>
+			<div className="breadcrumbs" />
+			<div className="product-content">
+				<div className="gallery">
+					<div className="main-img" />
+					<ul>
+						<li className="img" />
+						<li className="img" />
+						<li className="img" />
+					</ul>
+				</div>
+				<div className="product-info">
+					<h1>Bonsai</h1>
+					<span>$19.99</span>
+					<div className="description">
+						<p>
+							{
+								"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas in lacus enim. Donec ornare convallis diam. Phasellus volutpat dolor et ornare laoreet. Curabitur quis magna id dui interdum vehicula. Cras pharetra turpis quis ornare mollis. Sed enim ante, tincidunt ac rutrum nec, imperdiet nec lacus. Sed venenatis dolor nec tincidunt porta. Donec a ipsum pharetra."
+							}
+						</p>
+						<p>
+							{
+								"Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis interdum velit non tellus pharetra porttitor. Suspendisse tempus purus tellus, ac accumsan dolor finibus in. Suspendisse efficitur sed libero non ultrices. Pellentesque dignissim orci nec vehicula porttitor. Vestibulum ultricies augue id nulla feugiat auctor."
+							}
+						</p>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
